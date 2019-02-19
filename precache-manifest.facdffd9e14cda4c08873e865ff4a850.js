85f1eb63cca2dbfdc972ac899a15bc113e02b864
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-19 23:04:38 +0100

    Updates

---
+++ precache-manifest.facdffd9e14cda4c08873e865ff4a850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "810170901e716fb39250",
+    "url": "/e-notes/static/js/runtime~main.81017090.js"
+  },
+  {
+    "revision": "eb9262b98f0aad1a3273",
+    "url": "/e-notes/static/js/main.eb9262b9.chunk.js"
+  },
+  {
+    "revision": "27a91dfa13f4827e7c9e",
+    "url": "/e-notes/static/js/2.27a91dfa.chunk.js"
+  },
+  {
+    "revision": "eb9262b98f0aad1a3273",
+    "url": "/e-notes/static/css/main.ee8ff0ae.chunk.css"
+  },
+  {
+    "revision": "c9b745bed8cab16d0e79628da84c89ed",
+    "url": "/e-notes/index.html"
+  }
+];
