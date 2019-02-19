5b0d5e3ce233b81f99d89a00da0d161982df6a76
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-19 22:16:07 +0100

    style enhancement

--- src/Note.js
+++ src/Note.js
@@ -15,7 +15,21 @@ class Note extends Component {
         this.save = this.save.bind(this)
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
+        this.randomBetween = this.randomBetween.bind(this)
     }
+    
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0, window.innerHeight - 150, 'px'),
+            transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+        }
+    }
+    
+    randomBetween(x, y, s) {
+        return x + Math.ceil(Math.random() * (y-x)) + s
+    }
+    
     edit() {
        this.setState ({
            editing: true
@@ -34,7 +48,7 @@ class Note extends Component {
     
     renderForm() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <form onSubmit={this.save}>
                     <textarea ref={input => this._newText = input} />  
                     <button id="save"><FaSave /></button>
@@ -45,7 +59,7 @@ class Note extends Component {
     
     renderDisplay () {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.edit} id="edit"><FaEdit /></button>
